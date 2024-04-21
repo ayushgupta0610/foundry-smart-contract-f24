d5d2044a7e4e0c763aee48b0d4b5ff6af463c266
Ayush Gupta <7221434+ayushgupta0610@users.noreply.github.com>
2024-04-21 16:40:27 +0530

    Update raffle contract | pending changes

---
+++ src/Raffle.sol
@@ -0,0 +1,121 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-license-identifier: MIT
+pragma solidity ^0.8.20;
+
+import { VRFCoordinatorV2Interface } from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import { VRFConsumerBaseV2 } from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+/**
+ * @title A simple Raffle contract
+ * @author Ayush Gupta
+ * @notice This contract is for creating a simple raffle
+ * @dev Implements Chainlink VRFv2
+ */
+contract Raffle is VRFConsumerBaseV2 {
+    error Raffle_NotEnoughEthSent();
+    error Raffle_TransferFailed();
+    error Raffle_RaffleNotOpen();
+
+    enum RaffleState { OPEN, CALCULATING }
+
+    uint32 private constant NUM_WORDS = 1;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+
+    // @dev duration of the lottery before the winner is picked
+    uint256 private immutable i_interval;
+    uint256 private immutable i_entranceFee;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint64 private immutable i_subscriptionId;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+
+
+    address payable[] private s_players;
+    uint256 private s_lastTimestamp;
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
+
+    event EnteredRaffle(address indexed player);
+    event PickedWinner(address indexed winner);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        bytes32 keyHash,
+        uint32 callbackGasLimit,
+        uint64 subscriptionId,
+        address vrfCoordinatorAddress
+    ) VRFConsumerBaseV2(vrfCoordinatorAddress) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_gasLane = keyHash;
+        i_callbackGasLimit = callbackGasLimit;
+        i_subscriptionId = subscriptionId;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorAddress);
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimestamp = block.timestamp;
+    }
+
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughEthSent();
+        }
+        if (s_raffleState == RaffleState.OPEN)
+            revert Raffle_RaffleNotOpen();
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
+
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimestamp) < i_interval) {
+            revert();
+        }
+        s_raffleState = RaffleState.CALCULATING;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        uint256 playersLength = s_players.length;
+        uint256 winnerIndex = randomWords[0] % playersLength;
+        address payable winner = s_players[winnerIndex];
+        s_recentWinner = winner;
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimestamp = block.timestamp;
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle_TransferFailed();
+        }
+        emit PickedWinner(winner);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}
---
+++ test/RaffleTest.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { Test, console } from "forge-std/Test.sol";
+
+contract RaffleTest {
+
+    function setUp() public {
+        // Set up state variables
+    }
+
+    function testEnterRaffle() public {
+
+    }
+
+    function testPickWinner() public {
+
+    }
+}
